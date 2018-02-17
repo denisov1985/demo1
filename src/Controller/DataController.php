fac0c33ae551096db3ac619d11364c239776b1a1
denisov1985 <denisov1985@gmail.com>
2018-02-17 14:49:44 +0200

    asas

---
+++ src/Controller/DataController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dimon
+ * Date: 17.02.18
+ * Time: 14:33
+ */
+
+namespace App\Controller;
+
+
+class DataController
+{
+    public function demo()
+    {
+        die('it works');
+    }
+}
