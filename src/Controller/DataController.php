bac3ac0ed77ab57f9d341bb76daf561eb45636d1
denisov1985 <denisov1985@gmail.com>
2018-02-22 22:17:39 +0200

    dasda

--- src/Controller/DataController.php
+++ src/Controller/DataController.php
@@ -8,20 +8,106 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DataController
+class DataController extends Controller
 {
-    public function demo()
+
+    public function menu(Request $request)
     {
-        die('it works');
+
+        $menu = [
+            'News',
+            'Products',
+            'About'
+        ];
+        return $this->render('data/menu.html.twig', [
+            'menu' => $menu
+        ]);
+    }
+
+
+    /**
+     * @Route ("/index", name="index")
+     * @return string
+     */
+    public function index(Request $request)
+    {
+
+        dump($request->get('test'));
+        dump($this);
+        $title = 'News';
+
+        return $this->render(
+            'data/index.html.twig',
+            [
+                'title_test' => $title
+            ]
+        );
     }
 
     /**
-     * @Route("/", name="data_test")
+     * @Route ("/hi", name="hi")
+     * @return string
+     */
+    public function hi()
+    {
+        return $this->render('data/hi.html.twig');
+    }
+
+
+    public function demo(
+        Request $request,
+        SessionInterface $session)
+    {
+        //$session->set('userId', '1');
+
+        echo $session->get('userId', '1');
+
+        die('demo');
+      return $this->redirect('http://google.com');
+    }
+
+    /**
+     * @Route("/", name="data_test_1")
      */
     public function test()
     {
-        die('it works test');
+        return new Response('lalala');
+    }
+
+    /**
+     * @Route(
+     *     "/hello/{name}",
+     *     name="hello",
+     *     methods={"GET", "POST"},
+     *     requirements={"name"="[a-z]+"}
+     *     )
+     */
+    public function hello($name)
+    {
+        return new JsonResponse([
+            'greeting' => "Hello $name"
+        ]);
+    }
+
+    /**
+     * @Route(
+     *     "/hello/{name}",
+     *     name="hello_int",
+     *     methods={"GET", "POST"},
+     *     requirements={"name"="\d+"}
+     * )
+     */
+    public function helloInt($name)
+    {
+        return new JsonResponse([
+            'greeting' => "Hello from int $name"
+        ]);
     }
 }
